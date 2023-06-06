4ed716e9ada072a6926f395444621b9f47a11c70
pankaj511995 <kpankaj511995@gmail.com>
2023-06-06 12:16:17 +0530

    admin Pagination added

--- controller/admin.js
+++ controller/admin.js
@@ -2,7 +2,7 @@ const Restaurant=require('../models/restaurant')
 
 exports.AdminAnalysis=async(req,res)=>{
   try{
-    const pagenumber=req.params |0//can take page number form params or query 
+    const pagenumber=req.params.pagenumber |0//can take page number form params or query 
 
     const restaurant=await Restaurant.find(). select(['adminId','restaurantName','totalReview']).sort([['totalReview',-1]]).skip(pagenumber*10).limit(10)
 
